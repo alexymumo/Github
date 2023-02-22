87d420dfbac2eef2a2b97925f2b98fab20f2d375
Alex Mumo <alexymumo2000@gmail.com>
2023-02-22 23:21:59 +0300

    added remote module

--- app/src/main/java/com/codes/githubapp/data/remote/api/GithubApi.kt
+++ app/src/main/java/com/codes/githubapp/data/remote/api/GithubApi.kt
@@ -1,5 +1,8 @@
 package com.codes.githubapp.data.remote.api
 
+import com.codes.githubapp.data.remote.dto.FollowersDto
+import com.codes.githubapp.data.remote.dto.OwnerDto
+import com.codes.githubapp.data.remote.dto.RepositoriesDto
 import com.codes.githubapp.domain.models.Followers
 import com.codes.githubapp.domain.models.Owner
 import com.codes.githubapp.domain.models.Repositories
@@ -12,17 +15,17 @@ interface GithubApi {
     suspend fun searchUser(
         @Header("Authorization") accessToken: String = "we",
         @Path("username") username: String
-    ): Owner
+    ): OwnerDto
 
     @GET("users/{username}/following")
     suspend fun getUserFollowers(
         @Header("Authorization") accessToken: String = "Token",
         @Path("username") username: String
-    ): Followers
+    ): FollowersDto
 
     @GET("users/{username}/repos")
     suspend fun getUserRepositories(
         @Path("username") username: String,
         @Header("Authorization") accessToken: String = "Token_here"
-    ): Repositories
+    ): RepositoriesDto
 }
